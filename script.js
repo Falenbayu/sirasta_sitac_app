2e0d16f8ad075e089c8f2166610e7678e4ef3145
Falen Bayu A <86699460+Falenbayu@users.noreply.github.com>
2023-10-17 00:33:24 +0800

    new

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ function fetchApi(detail_pica){
     wrapper.classList.remove("active");
     infoText.style.color = "#000";
     infoText.innerHTML = `Mencari Solusi dari <span>"${detail_pica}"</span>`;
-    let url = `https://sheetdb.io/api/v1/kmd3utdyghxjr`;
+    let url = ``;
     fetch(url).then(response => response.json()).then(result => data(result)).catch(() =>{
         infoText.innerHTML = `Tidak dapat menemukan solusi <span>"${data}"</span>. Tolong, coba cari kata lain.`;
     });
